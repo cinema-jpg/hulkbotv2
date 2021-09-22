5443d54e2358e7156d4cbf54074f9b6d764c6915
cinema-jpg <85360750+cinema-jpg@users.noreply.github.com>
2021-09-23 00:12:30 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_name} \n @cinemazipnews",
+                caption = f"<code>{file_name}</code> \n @CINEMAZIPNEWS",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
